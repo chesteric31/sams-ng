7bda3acefa0838b51c8642db21c215816ffc3751
chesteric31 <chesteric31@gmail.com>
2018-04-20 14:51:52 +0200

    Structure with nav, foot and home components.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,12 +7,18 @@ import {HttpClientModule} from '@angular/common/http';
 import { AppRoutingModule } from './/app-routing.module';
 import { CategoryComponent } from './category/category.component';
 import {CategoryService} from './category/category.service';
+import { NavComponent } from './nav/nav.component';
+import { FootComponent } from './foot/foot.component';
+import { HomeComponent } from './home/home.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    CategoryComponent
+    CategoryComponent,
+    NavComponent,
+    FootComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
